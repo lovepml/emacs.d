52208aa4288ef7358d9e68c66f2094ba021a9c1a
emuio <emuio@emuio.local>
2017-06-02 09:39:20 +0800

    Revert "Enable flycheck-color-mode-line-mode"
    This reverts commit c1d46ec3837708e79b0ce3a8efebfe2eefe6f4d0.

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,9 +1,6 @@
 (when (maybe-require-package 'flycheck)
   (add-hook 'after-init-hook 'global-flycheck-mode)
-  (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
-
-  (when (maybe-require-package 'flycheck-color-mode-line)
-    (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
+  (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list))
 
 
 (provide 'init-flycheck)

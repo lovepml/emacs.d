a01b34b901755df38c68aa7de6f74ce3e4b60e47
emuio <emuio@emuio.local>
2017-06-02 14:33:54 +0800

    add force revert buffer and bind to <f5>

--- lisp/init-custom-function.el
+++ lisp/init-custom-function.el
@@ -26,4 +26,17 @@
   (while (search-forward "\r" nil t) (replace-match ""))
   (whitespace-cleanup))
 
+;; force revert buffer
+(global-set-key
+ (kbd "<f5>")
+ (lambda (&optional force-reverting)
+   "Interactive call to revert-buffer. Ignoring the auto-save
+ file and not requesting for confirmation. When the current buffer
+ is modified, the command refuses to revert it, unless you specify
+ the optional argument: force-reverting to true."
+   (interactive "P")
+   (if (or force-reverting (not (buffer-modified-p)))
+       (revert-buffer :ignore-auto :noconfirm)
+     (error "The buffer has been modified"))))
+
 (provide 'init-custom-function)
